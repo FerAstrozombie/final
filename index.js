9eff257d6eeee24265afce9ef7a1206b4a8bc407
flair <fernando_punk77@hotmail.com>
2022-07-06 09:25:34 -0300

    agregando arrays en js

--- index.js
+++ index.js
@@ -32,4 +32,45 @@ function calculadoraEnvios(zona){
 
 }
 let zona = prompt("Ingrese zona de residencia (zona oeste, norte, sur, este)").toLowerCase();
-alert(calculadoraEnvios(zona));            
+alert(calculadoraEnvios(zona)); 
+
+class Producto{
+  constructor(img, nombre, precio){
+      this.img = img;
+      this.nombre = nombre;
+      this.precio = precio;
+      this.vendido = false;
+  }
+
+  vender(){
+      this.vendido = true;
+  }
+}
+
+let productos = [];
+
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/goku%201.jpg", "Remera Goku 1", 1200));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/goku%202.jpg", "Remera Goku 2", 1200));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/goku%203.jpg", "Remera Goku 3", 1200));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/goku%203.jpg", "Remera Goku 3", 1200));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/goku%204.jpg", "Remera Goku 4", 1200));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/goku%206.jpg", "Remera Goku 5", 1200));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/goku%207.jpg", "Remera Goku 6", 1200));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/breacking%20bad.jpg", "Remera Breacking bad 1", 1300));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/breacking%20bad%202.jpg", "Remera Breacking bad 2", 1300));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/breacking%20bad%203.jpg", "Remera Breacking bad 3", 1300));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/mario%20bross%201.jpg", "Remera Mario bross 1", 1250));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/mario%20bross%202.jpg", "Remera Mario bross 2", 1250));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/mario%20bross%203.jpg", "Remera Mario bross 3", 1250));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/mario%20bross%204.jpg", "Remera Mario bross 4", 1250));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/volver%20al%20futuro%201.jpg", "Remera Volver al futuro 1", 1350));
+productos.push(new Producto("http://127.0.0.1:5500/imagenes/volver%20al%20futuro%202.jpg", "Remera Volver al futuro 1", 1350));
+
+// con el if se cambia el estado de vendido de false a true
+for(const item of productos){
+  if(item.nombre === "Remera Goku 1"){
+      item.vender()
+  }
+}
+
+console.log(productos);
